3657b3b84908f9b98fc16dff2a451fd19a0f3c4d
kayabilal <kayabilal505@gmail.com>
2022-07-28 12:35:23 +0300

    Required property

--- src/main/java/com/bilal/kaya/controller/HelloController.java
+++ src/main/java/com/bilal/kaya/controller/HelloController.java
@@ -47,10 +47,29 @@ public class HelloController {
         return "Your message is:" +message;
     }*/
 
-    @GetMapping("/message")
+    /*@GetMapping("/message")
     public String getMyMessage(@RequestParam("m")  String message){
         return "Your message is:" +message;
+    }*/
+
+    @GetMapping({"/message1","/message1/{message}"})
+    public String getMyMessageWithVariable(@PathVariable(name="message",required = false) String message){
+        return "Your message is :" + message;
     }
+
+
+    @GetMapping("/message2")
+    public String getMyMessageWithParam(@RequestParam(name = "message",required = false,defaultValue ="my default message") String message){
+        return "Your message is: " +message;
+    }
+
+
+
+
+
+
+
+
 
 
 
