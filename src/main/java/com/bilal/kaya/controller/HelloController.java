d98745247384fe6b118c475d3a8d79ae9392791f
kayabilal <kayabilal505@gmail.com>
2022-07-28 10:49:27 +0300

    @RequestMapping

--- src/main/java/com/bilal/kaya/controller/HelloController.java
+++ src/main/java/com/bilal/kaya/controller/HelloController.java
@@ -2,13 +2,18 @@ package com.bilal.kaya.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/api")
 public class HelloController {
 
 
     @GetMapping(path="/hello")
+    //@RequestMapping(path = "/hello",method = RequestMethod.GET)
+    // 13 ve 14. satırdaki kodlar aynı şeyi ifade eder.ikiside kullanılabilir.Genelde getmapping kullanılır çünkü daha anlaşılır.
+    // burda görüldüğü üzere @RequestMapping hem class hem de method seviyesinde kullanılabilir.
     public String sayHello(){
         return "Hello World";
     }
