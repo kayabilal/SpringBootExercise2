64ba523f274e93def9a4e4d1f43fb31bf315d10a
kayabilal <kayabilal505@gmail.com>
2022-07-28 11:47:10 +0300

    @PathVariable

--- src/main/java/com/bilal/kaya/controller/HelloController.java
+++ src/main/java/com/bilal/kaya/controller/HelloController.java
@@ -13,8 +13,7 @@ public class HelloController {
     // DELETE: We use it when we want to delete data.  @DeleteMapping
 
 
-
-    @GetMapping(path="/hello")
+    /*@GetMapping(path="/hello")
     //@RequestMapping(path = "/hello",method = RequestMethod.GET)
     // 13 ve 14. satırdaki kodlar aynı şeyi ifade eder.ikiside kullanılabilir.Genelde getmapping kullanılır çünkü daha anlaşılır.
     // burda görüldüğü üzere @RequestMapping hem class hem de method seviyesinde kullanılabilir.
@@ -28,5 +27,22 @@ public class HelloController {
     @DeleteMapping("/delete")
     public String delete(){
         return "Data deleted";
+    }*/
+
+
+
+    // @PathVariable'ın iki kullanımı var.ikiside görüldüğü üzere aşağıdadır.
+    @GetMapping("/message/{message}")
+    public String getMessage(@PathVariable String message){
+        return "Your message is : "+message;
     }
+    /*@GetMapping("/message/{m}")
+    public String getMessage2(@PathVariable("m") String message ){
+        return "Your message is : "+message;
+    }*/
+
+
+
+
+
 }
